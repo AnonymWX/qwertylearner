be552571695018f6232df31a1bdb70150b66ccdb
AnonymWX <anonymwxy@gmail.com>
2026-09-24 18:03:25 +0700

    Update index.tsx

--- src/pages/Typing/components/WordPanel/components/InputHandler/index.tsx
+++ src/pages/Typing/components/WordPanel/components/InputHandler/index.tsx
@@ -20,7 +20,7 @@ export default function InputHandler({ updateInput }: { updateInput: (updateObj:
       case 'code':
         return <TextAreaHandler updateInput={updateInput} />
       case 'vi':
-        return <TextAreaHandler updateInput={updateInput} />
+        return <VietnameseKeyEventHandler updateInput={updateInput} />
       default:
         return <TextAreaHandler updateInput={updateInput} />
     }
