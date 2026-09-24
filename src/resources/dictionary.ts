1130cf1aceda3e891a74299528544b47b524df19
AnonymWX <anonymwxy@gmail.com>
2026-09-24 13:51:42 +0700

    Change language from 'vi' to 'en' in dictionary

--- src/resources/dictionary.ts
+++ src/resources/dictionary.ts
@@ -4109,7 +4109,7 @@ const vietnamExam: DictionaryResource[] = [
     tags: ['越南语', '基础'],
     url: '/dicts/vietnamese_basic.json',
     length: 3,
-    language: 'vi',
+    language: 'en',
   },
 ]
 /**
