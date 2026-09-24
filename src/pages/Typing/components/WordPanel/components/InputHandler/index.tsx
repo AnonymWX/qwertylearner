72425aeafff65eb0030f74d478e2fea2cbd613d6
AnonymWX <anonymwxy@gmail.com>
2026-09-24 17:04:26 +0700

    Refactor InputHandler to remove Vietnamese input support
    Removed Vietnamese input method handling from InputHandler.

--- src/pages/Typing/components/WordPanel/components/InputHandler/index.tsx
+++ src/pages/Typing/components/WordPanel/components/InputHandler/index.tsx
@@ -1,30 +1,14 @@
 import KeyEventHandler from '../KeyEventHandler'
 import TextAreaHandler from '../TextAreaHandler'
 import { currentDictInfoAtom } from '@/store'
-import { VietnameseInput } from 'gotiengviet'
 import { useAtomValue } from 'jotai'
 import type { FormEvent } from 'react'
-import { useEffect, useMemo } from 'react'
+import {  useMemo } from 'react'
 
 export default function InputHandler({ updateInput }: { updateInput: (updateObj: WordUpdateAction) => void }) {
   const dictInfo = useAtomValue(currentDictInfoAtom)
 
-  // 当词典是越南语时，启用 gotiengviet 输入法
-  useEffect(() => {
-    if (dictInfo.language === 'vi') {
-      VietnameseInput.getInstance({
-        inputMethod: 'telex',
-        enabled: true,
-      })
-    }
-    return () => {
-      if (dictInfo.language === 'vi') {
-        VietnameseInput.destroyInstance()
-      }
-    }
-  }, [dictInfo.language])
-
-  const handler = useMemo(() => {
+    const handler = useMemo(() => {
     switch (dictInfo.language) {
       case 'en':
         return <KeyEventHandler updateInput={updateInput} />
