1a1b7d03f5ed9e7c2e747d9457a8a60fc2d1620f
AnonymWX <anonymwxy@gmail.com>
2026-09-26 13:31:28 +0700

    Refactor InputHandler component by removing unused code

--- src/pages/Typing/components/WordPanel/components/InputHandler/index.tsx
+++ src/pages/Typing/components/WordPanel/components/InputHandler/index.tsx
@@ -13,71 +13,7 @@ export default function InputHandler({
 }: {
   updateInput: (updateObj: WordUpdateAction) => void
   wordName: string
-  viResetSignal?: numberimport KeyEventHandler from '../KeyEventHandler'
-import TextAreaHandler from '../TextAreaHandler'
-import VietnameseKeyEventHandler from '../VietnameseKeyEventHandler'
-import { currentDictInfoAtom } from '@/store'
-import { useAtomValue } from 'jotai'
-import type { FormEvent } from 'react'
-import { useMemo } from 'react'
-
-export default function InputHandler({
-  updateInput,
-  wordName,
-  viResetSignal,
-}: {
-  updateInput: (updateObj: WordUpdateAction) => void
-  wordName: string
   viResetSignal?: number
-}) {
-  const dictInfo = useAtomValue(currentDictInfoAtom)
-
-  const handler = useMemo(() => {
-    switch (dictInfo.language) {
-      case 'en':
-        return <KeyEventHandler updateInput={updateInput} />
-      case 'de':
-        return <KeyEventHandler updateInput={updateInput} />
-      case 'romaji':
-        return <KeyEventHandler updateInput={updateInput} />
-      case 'code':
-        return <TextAreaHandler updateInput={updateInput} />
-      case 'vi':
-        return <VietnameseKeyEventHandler updateInput={updateInput} wordName={wordName} viResetSignal={viResetSignal} />
-      default:
-        return <TextAreaHandler updateInput={updateInput} />
-    }
-  }, [dictInfo.language, updateInput, wordName, viResetSignal])
-
-  return <>{handler}</>
-}
-
-export type WordUpdateAction = WordAddAction | WordDeleteAction | WordCompositionAction | WordReplaceAction | WordRejectAction
-
-export type WordAddAction = {
-  type: 'add'
-  value: string
-  event: FormEvent<HTMLTextAreaElement> | KeyboardEvent
-}
-
-export type WordDeleteAction = {
-  type: 'delete'
-  length: number
-}
-
-export type WordCompositionAction = {
-  type: 'composition'
-  value: string
-}
-
-export type WordReplaceAction = {
-  type: 'replace'
-  value: string
-}
-
-export type WordRejectAction = {
-  type: 'reject'
-}
 }) {
   const dictInfo = useAtomValue(currentDictInfoAtom)
 
